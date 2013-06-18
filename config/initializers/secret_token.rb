5b0ef647c98459adf44a958ac7dcc2ced5527c66
chintanparikh <chintanparikh@gatech.edu>
2013-06-18 19:53:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oyster::Application.config.secret_token = '77df755f3b35c777779f7a0df67256e2f97bd6f13c4b427f604faeea954ee4c6333ceabc314f42ac06e616d6357e1b6c887443591d9be0c9d5c4bd6ca2538e51'
