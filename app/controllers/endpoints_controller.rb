f6dbd3d0e1151c6b0c5041ec3f4ef72d4b43c011
chintanparikh <chintanparikh@gatech.edu>
2013-06-19 00:46:36 -0400

    Added controller action

--- app/controllers/endpoints_controller.rb
+++ app/controllers/endpoints_controller.rb
@@ -1,14 +1,15 @@
 class EndpointsController < ApplicationController
   def index
-  end
-
-  def new
+    # Empty action/splash page with link to create a new endpoint
   end
 
   def create
+    @endpoint = Endpoint.create
+    redirect_to @endpoint
   end
 
   def show
+    @endpoint = Endpoint.find(params[:id])
   end
 
   def destroy
