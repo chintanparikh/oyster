bcc38d25f9f09ebd8f12b159a8c2a1ea999d2fd3
chintanparikh <chintanparikh@gatech.edu>
2013-06-19 00:46:24 -0400

    Added endpoint controller specs

--- spec/controllers/endpoints_controller_spec.rb
+++ spec/controllers/endpoints_controller_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe EndpointsController do
 
+  describe "#index" do
+    subject { get :index }
+    it { should be_successful }
+  end
+
+  describe "#create" do
+    let(:params) { {} }
+    subject { post :create, params }
+    it "should create a new endpoint" do
+      expect { subject }.to change{ Endpoint.count }.by(1)
+    end
+    
+    it "should redirect to the correct endpoint" do
+      subject.should redirect_to assigns[:endpoint].id
+    end
+  end
+
 end
