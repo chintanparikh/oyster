5f22d97d6d9c52adcb461df78837b6912466f5b8
chintanparikh <chintanparikh@gatech.edu>
2013-07-12 13:18:52 -0400

    Add method to get newest content

--- app/controllers/endpoints_controller.rb
+++ app/controllers/endpoints_controller.rb
@@ -17,6 +17,27 @@ class EndpointsController < ApplicationController
     show_endpoint Endpoint.find_by_token params[:token]
   end
 
+  def new_content
+    @endpoint = Endpoint.find(params[:endpoint_id])
+    last_content = @endpoint.content.last
+
+    if last_content.id <= params[:last_content_id].to_i
+      new_content = {content: nil}
+    else
+      content = @endpoint.content.where("id > #{params[:last_content_id]}")
+      #new_content looks like
+      # { content: 
+      #   {
+      #     5: "{key: value}",
+      #     7: "{another_key: another_value}"      
+      #   }
+      # }
+      new_content = {content: Hash[content.collect { |v| [v.id, v.content] }]}
+    end
+
+    render json: new_content.to_json
+  end
+
   protected
   # DRYing up the code a little
   def show_endpoint endpoint
