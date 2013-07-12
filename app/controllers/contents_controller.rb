3b7ea2ab1b16328a978f619dade05278ced6c460
chintanparikh <chintanparikh@gatech.edu>
2013-07-12 13:18:29 -0400

    Bugfixes

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,9 +1,11 @@
 class ContentsController < ApplicationController
 
   def create
-    [:controller, :action, :token].each do |attribute|
+    [:controller, :action].each do |attribute|
       params.delete attribute
     end
+
+    token = params.delete :token
     
     endpoint = Endpoint.find_by_token(token)
     content = Content.create(endpoint_id: endpoint.id, content: params.to_json)
